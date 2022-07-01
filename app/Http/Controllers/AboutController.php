d3167d1c8f91d1057d21eaeab71602701052f9ac
maryom99 <99712544+maryom99@users.noreply.github.com>
2022-07-01 13:34:53 +0700

    Prakticum 2_lastpart_Jobsheet 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        echo    "Name: Maryam Mohammed Ali Albaiti <br>";
+        echo    "NIM: 2041720205 <br>";
+        echo    "Class: TI-2I";
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController; 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\AboutController; 
+use App\Http\Controllers\ArticleController; 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,8 +19,8 @@ use App\Http\Controllers\PageController;
 
 // Practicum 2
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/articles', [PageController::class, 'articles($id)']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles', [ArticleController::class, 'articles($id)']);
 
 
